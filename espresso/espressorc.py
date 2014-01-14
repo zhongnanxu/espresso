f840ee44da9bd7fe7e1325ab6aef106794469b18
Zhongnan Xu <zhongnanxu@cmu.edu>
2014-01-14 18:01:18 +0100

    Changed rc to reflect gilgamesh environment

--- espresso/espressorc.py
+++ espresso/espressorc.py
@@ -2,5 +2,5 @@
 """This module contains the computer specific variables
 """
 
-ESPRESSORC = {'executable':'/home/opt/el6/dl160g6/espresso-5.0.3-dl160g6-tm-gfortran-openmpi-1.6.3-acml-4.4.0-1/bin/pw.x',
-              'PPpath': '/home/camp/zhonxu/pseudo'}
+ESPRESSORC = {'executable':'/home-research/zhongnanxu/opt/espresso-5.0/bin/pw.x',
+              'PPpath': '/home-research/zhongnanxu/opt/espresso-5.0/pseudo'}
