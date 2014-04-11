e35a9e0793044eb547fbcd66d562ec4934b0514e
Zhongnan Xu <zhongnanxu@cmu.edu>
2014-04-11 15:50:27 -0400

    Changed rundir to reflect gilgamesh environment

--- espresso/espressorc.py
+++ espresso/espressorc.py
@@ -5,4 +5,4 @@
 ESPRESSORC = {'executable':'/home-research/zhongnanxu/opt/espresso-5.0.2-ifort-internal-lapack/bin/pw.x',
               'PPpath': '/home-research/zhongnanxu/gbrv_espresso_pseudo',
               'mpicmd': 'mpirun',
-              'rundir': '/tmp/PBS${PBS_JOBID}'}  # use ./ as default
+              'rundir': '/scratch/PBS${PBS_JOBID}'}  # use ./ as default
